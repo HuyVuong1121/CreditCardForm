25e3960a0fb682a381faa5182d49ee887e13721c
Bruce McTigue <bmctigue@tiguer.com>
2016-07-15 11:49:53 -0600

    First updates for the form itself.

--- CreditCardForm/controllers/ViewController.swift
+++ CreditCardForm/controllers/ViewController.swift
@@ -11,12 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var cardNumberLabel: UILabel!
+    @IBOutlet weak var cardNumberTextField: UITextField!
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var expirationDateLabel: UILabel!
+    @IBOutlet weak var expirationDateTextField: UITextField!
+    @IBOutlet weak var cvvLabel: UILabel!
+    @IBOutlet weak var cvvTextField: UITextField!
+    @IBOutlet weak var cvvImageView: UIImageView!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = Theme.sharedInstance.darkThemeColor()
-        titleLabel.textColor = Theme.sharedInstance.contrastThemeColor()
+        Theme.sharedInstance.setNavigationBarAppearance(navigationController)
+        self.view.backgroundColor = Theme.sharedInstance.lightThemeColor()
+        let image = UIImage(named: "Venmo_Logo.png")
+        self.navigationItem.titleView = UIImageView(image: image)
+        
+        setupControls()
+    }
+    
+    func setupControls() {
+        titleLabel.textColor = Theme.sharedInstance.textThemeColor()
+        cardNumberLabel.textColor = Theme.sharedInstance.textThemeColor()
+        expirationDateLabel.textColor = Theme.sharedInstance.textThemeColor()
+        cvvLabel.textColor = Theme.sharedInstance.textThemeColor()
+        submitButton.layer.cornerRadius = 4.0
+        submitButton.backgroundColor = Theme.sharedInstance.darkThemeColor()
+        submitButton.setTitleColor(Theme.sharedInstance.contrastThemeColor(), forState: UIControlState.Normal)
+        containerView.layer.cornerRadius = 4.0
+        containerView.layer.borderWidth = 1.0
+        containerView.layer.borderColor = Theme.sharedInstance.darkThemeColor().CGColor
     }
 
+    @IBAction func submitButtonPressed(sender: AnyObject) {
+        
+    }
 }
 
--- CreditCardForm/lib/Theme.swift
+++ CreditCardForm/lib/Theme.swift
@@ -13,15 +13,26 @@ class Theme {
     static let sharedInstance = Theme()
     
     func darkThemeColor() -> UIColor {
-        return UIColor.init(red: 0.06, green: 0.29, blue: 0.46, alpha: 1.0)
+        return UIColor.init(red: 49.0/255.0, green: 127.0/255.0, blue: 194.0/255.0, alpha: 1.0)
     }
     
     func lightThemeColor() -> UIColor {
-        return UIColor.init(red: 0.09, green: 0.45, blue: 0.71, alpha: 1.0)
+        return UIColor.init(red: 226.0/255.0, green: 230.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+    }
+    
+    func textThemeColor() -> UIColor {
+        return UIColor.init(red: 84.0/255.0, green: 95.0/255.0, blue: 102.0/255.0, alpha: 1.0)
     }
     
     func contrastThemeColor() -> UIColor {
         return UIColor.whiteColor()
+    }
+    
+    func setNavigationBarAppearance(navigationController: UINavigationController?) {
+        if let controller = navigationController {
+            controller.navigationBar.barTintColor = darkThemeColor()
+            controller.navigationBar.tintColor = UIColor.whiteColor()
+        }
     }
 }
 
