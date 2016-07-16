877dd079bdf9ac04e5e79d6391e5733fedea3b61
Bruce McTigue <bmctigue@tiguer.com>
2016-07-15 19:53:10 -0600

    update indenting

--- CreditCardForm/models/CreditCard.swift
+++ CreditCardForm/models/CreditCard.swift
@@ -14,54 +14,54 @@ enum CreditCardType {
     
     var logo: String {
         switch self {
-        case .Amex:
-            return "Cards_Amex.png"
-        case .DinersClub:
-            return "Cards_Dinerclub.png"
-        case .Discover:
-            return "Cards_Discover.png"
-        case .JCB:
-            return "Cards_JCB.png"
-        case .MasterCard:
-            return "Cards_Mastercard.png"
-        case .Visa:
-            return "Cards_Visa.png"
-        case .Unknown:
-            return "Cards_Blank.png"
+            case .Amex:
+                return "Cards_Amex.png"
+            case .DinersClub:
+                return "Cards_Dinerclub.png"
+            case .Discover:
+                return "Cards_Discover.png"
+            case .JCB:
+                return "Cards_JCB.png"
+            case .MasterCard:
+                return "Cards_Mastercard.png"
+            case .Visa:
+                return "Cards_Visa.png"
+            case .Unknown:
+                return "Cards_Blank.png"
         }
     }
     
     var regex: String {
         switch self {
-        case .Amex:
-            return "^3[47][0-9]{4,}$"
-        case .DinersClub:
-            return "^3(?:0[0-5]|[68][0-9])[0-9]{3,}$"
-        case .Discover:
-            return "^6(?:011|5[0-9]{2})[0-9]{2,}$"
-        case .JCB:
-            return "^(?:2131|1800|35[0-9]{3})[0-9]{1,}$"
-        case .MasterCard:
-            return "^5[1-5][0-9]{4,}$"
-        case .Visa:
-            return "^4[0-9]{5,}$"
-        case .Unknown:
-            return "^$"
+            case .Amex:
+                return "^3[47][0-9]{4,}$"
+            case .DinersClub:
+                return "^3(?:0[0-5]|[68][0-9])[0-9]{3,}$"
+            case .Discover:
+                return "^6(?:011|5[0-9]{2})[0-9]{2,}$"
+            case .JCB:
+                return "^(?:2131|1800|35[0-9]{3})[0-9]{1,}$"
+            case .MasterCard:
+                return "^5[1-5][0-9]{4,}$"
+            case .Visa:
+                return "^4[0-9]{5,}$"
+            case .Unknown:
+                return "^$"
         }
     }
     
     var cardNumberLength: Int {
         switch self {
-        case .DinersClub: return 14
-        case .Amex: return 15
-        case .Discover, .JCB, .MasterCard, .Visa, .Unknown: return 16
+            case .DinersClub: return 14
+            case .Amex: return 15
+            case .Discover, .JCB, .MasterCard, .Visa, .Unknown: return 16
         }
     }
     
     var cvvLength: Int {
         switch self {
-        case .Amex: return 4
-        case .DinersClub, .Discover, .JCB, .MasterCard, .Visa, .Unknown: return 3
+            case .Amex: return 4
+            case .DinersClub, .Discover, .JCB, .MasterCard, .Visa, .Unknown: return 3
         }
     }
     
