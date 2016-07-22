7cabb0a20834c8d72e857237acfa5d01040646e0
Bruce McTigue <bmctigue@tiguer.com>
2016-07-22 11:03:04 -0600

    Added blank tests for evaluator

---
+++ CreditCardFormTests/CreditCardEvaluatorTests.swift
@@ -0,0 +1,27 @@
+//
+//  CreditCardEvaluatorTests.swift
+//  CreditCardForm
+//
+//  Created by Bruce McTigue on 7/22/16.
+//  Copyright © 2016 tiguer. All rights reserved.
+//
+
+import XCTest
+import UIKit
+@testable import CreditCardForm
+
+class CreditCardEvaluatorTests: XCTestCase, CreditCardEvaluator {
+
+    func testEvaluateCardNumber() {
+        
+    }
+    
+    func testEvaluateExpiredDate() {
+        
+    }
+    
+    func testEvaluateCVV() {
+        
+    }
+
+}
